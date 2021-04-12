29215299b96764796a3c9197fe2d339c751cf7bb
Nekro <80943379+pzps94038@users.noreply.github.com>
2021-04-12 11:56:42 +0800

    Add files via upload

---
+++ Person/Person/Person/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    class Person
+    {
+        private string name;
+        public string Title
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Hello! {Title}");
+        }
+
+    }
+}
---
+++ Person/Person/Person/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person chris = new Person();
+            Console.Write("請輸入你的名字: ");
+            chris.Title = Console.ReadLine();
+            chris.Display();
+            Console.ReadKey();
+        }
+
+    }
+}
